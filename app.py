8cb4e632d2e4795580d238bf0a06418ffe7d4676
DomenickD <deothe1@gmail.com>
2025-02-13 11:42:52 -0500

    streamlit added

---
+++ app.py
@@ -0,0 +1,15 @@
+"""This is a test file for the ABDD project."""
+
+import streamlit as st
+
+st.title("ABDD Test")
+
+st.header("This is a header")
+st.subheader("This is a subheader")
+st.write("This is a write text")
+st.markdown("This is a markdown text")
+st.latex(r"y = \frac{a}{b}")
+st.success("This is a success message")
+st.error("This is an error message")
+st.warning("This is a warning message")
+st.info("This is an info message")
